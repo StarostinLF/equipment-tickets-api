320f9b6d30a75ef9dc015b7e6d4edbccf32871c4
StarostinLF <starostinlf@mail.ru>
2026-09-17 17:06:27 +0300

    Подключены роуты заявок, включая вложенный ресурс и смену статуса

---
+++ src/routes/requestRoutes.js
@@ -0,0 +1,56 @@
+import { Router } from 'express';
+import { requestController } from '../controllers/requestController.js';
+import { validate } from '../validators/validate.js';
+import {
+  createRequestSchema,
+  updateRequestSchema,
+  updateRequestStatusSchema,
+  requestIdParamsSchema,
+  listRequestsQuerySchema,
+  listRequestsByEquipmentQuerySchema,
+} from '../validators/requestSchemas.js';
+import { equipmentIdParamsSchema } from '../validators/equipmentSchemas.js';
+
+export const requestRouter = Router();
+
+requestRouter.get(
+  '/requests',
+  validate({ query: listRequestsQuerySchema }),
+  requestController.list,
+);
+
+requestRouter.post(
+  '/requests',
+  validate({ body: createRequestSchema }),
+  requestController.create,
+);
+
+requestRouter.get(
+  '/requests/:id',
+  validate({ params: requestIdParamsSchema }),
+  requestController.getById,
+);
+
+requestRouter.patch(
+  '/requests/:id',
+  validate({ params: requestIdParamsSchema, body: updateRequestSchema }),
+  requestController.update,
+);
+
+requestRouter.patch(
+  '/requests/:id/status',
+  validate({ params: requestIdParamsSchema, body: updateRequestStatusSchema }),
+  requestController.updateStatus,
+);
+
+requestRouter.delete(
+  '/requests/:id',
+  validate({ params: requestIdParamsSchema }),
+  requestController.remove,
+);
+
+requestRouter.get(
+  '/equipment/:id/requests',
+  validate({ params: equipmentIdParamsSchema, query: listRequestsByEquipmentQuerySchema }),
+  requestController.listByEquipment,
+);
