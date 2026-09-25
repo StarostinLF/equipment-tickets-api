9fc1e1465f0118a67c4d261009760ae2992493df
StarostinLF <starostinlf@mail.ru>
2026-09-25 13:01:44 +0300

    Добавлены репозитории списков площадок и специалистов

--- src/repositories/technicianRepository.js
+++ src/repositories/technicianRepository.js
@@ -1,6 +1,24 @@
 import { Technician } from '../db/models/index.js';
 
+function toDto(technician) {
+  const plain = technician.get({ plain: true });
+  return {
+    id: plain.id,
+    fullName: plain.fullName,
+    specialization: plain.specialization,
+    personnelNumber: plain.personnelNumber,
+  };
+}
+
 export const technicianRepository = {
+  async findAll() {
+    const technicians = await Technician.findAll({
+      attributes: ['id', 'fullName', 'specialization', 'personnelNumber'],
+      order: [['fullName', 'ASC']],
+    });
+    return technicians.map(toDto);
+  },
+
   async findAllByIds(ids, transaction) {
     return Technician.findAll({ where: { id: ids }, attributes: ['id'], transaction });
   },
